90ccc36c76202b6e5308bb3b7cf88bdc2de6e72a
Edrisa Jobe <ed.jobe@yahoo.com>
2022-12-25 00:39:21 -0500

    book page

--- app/views.py
+++ app/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def home(request):
     
     return render(request, 'app/pages/home.html')
+
+def book(request):
+
+    return render(request, 'app/pages/book.html')
