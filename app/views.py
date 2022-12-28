819b4179ae2486f605f67b754bfb25a410c3b616
ejobe1 <ed.jobe@yahoo.com>
2022-12-28 15:49:11 -0500

    comments

--- app/views.py
+++ app/views.py
@@ -30,7 +30,8 @@ def home(request):
 
 
 def flight_agenda(request):
-
+    
+    # grab all the saved items within database
     flight_list = Flight.objects.all()
 
     if request.method == "POST":
@@ -38,12 +39,14 @@ def flight_agenda(request):
 
         if form.is_valid():
 
+            # get each individual item
             flight_number = form.cleaned_data['flight_number']
             flight_origin = form.cleaned_data['flight_origin']
             flight_destination = form.cleaned_data['flight_destination']
             flight_arrival = form.cleaned_data['flight_arrival']
             flight_return = form.cleaned_data['flight_return']
 
+            # setting the varibales within themselves and saving the form
             Flight.objects.filter(flight_number=flight_number, flight_origin=flight_origin,
                                   flight_destination=flight_destination, flight_arrival=flight_arrival, flight_return=flight_return)
             form.save()
